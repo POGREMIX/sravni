7bda3b9571dd4f68efb73e2256d1be734d67f88e
pogremix <pogremix@yandex.ru>
2020-09-28 15:26:33 +0300

    add 500 error page to main

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 from flask import Flask
 from flask import render_template, url_for
 from flask_bootstrap import Bootstrap
+
+from routes.error_route import error_route
 from routes.text_route import text_route
 from routes.index_route import index_route
 from routes.user_route import user_route
@@ -13,6 +15,7 @@ bootstrap = Bootstrap(application)
 application.register_blueprint(text_route)
 application.register_blueprint(index_route)
 application.register_blueprint(user_route)
+application.register_blueprint(error_route)
 
 
 if __name__ == '__main__':
