d87f68e2234eea812f2287f46c81350ac905801a
pogremix <pogremix@yandex.ru>
2020-09-30 14:18:03 +0300

    change login route back

--- routes/user_route.py
+++ routes/user_route.py
@@ -1,9 +1,6 @@
 from flask import Blueprint, redirect, url_for, flash
 from flask import render_template
-
-
 from forms import ContactForm
-from werkzeug.datastructures import MultiDict
 
 user_route = Blueprint('user_route', __name__)
 
@@ -13,10 +10,11 @@ def user(user_id):
     return render_template('user.html', user_id=user_id)
 
 
-@user_route.route('/user/auth/', methods=['get', 'post'])
-def auth():
+@user_route.route('/user/login/', methods=['get', 'post'])
+def login():
     form = ContactForm()
     if form.validate_on_submit():
+        
         name = form.name.data
         email = form.email.data
         message = form.message.data
